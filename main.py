497b381c54848dc7ac8bcfb2a98f89f9110fba57
taniyeahh7 <taniya.ahmed734@gmail.com>
2023-09-23 14:54:17 +0530

    visualized landmarks

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ while True:
     #just to detect face 
     rgb_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     output=face_mesh.process(rgb_frame)
-    
     #creates lots of points
     landmark_points=output.multi_face_landmarks
+    frame_h,frame_w,_=frame.shape#tells the width height and width
+    if landmark_points:
+        landmarks=landmark_points[0].landmark
+        for landmark in landmarks:
+            #going to draw need to get the coordinates
+            x=int(landmark.x*frame_w)#has x y and z
+            y=int(landmark.y*frame_h)
+            cv2.circle(frame,(x,y),3,(0,255,0))
+            # z=landmark.z
+            print(x,y)
     print(landmark_points)
     cv2.imshow("eye controlled mouse",frame)
     cv2.waitKey(1)
